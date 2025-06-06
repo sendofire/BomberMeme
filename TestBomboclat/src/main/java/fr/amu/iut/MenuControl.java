871d86c02890ffff82aa9d1857ebdfaee61ea45d
t24020961 <esteban.toro@etu.univ-amu.fr>
2025-06-06 15:56:35 +0200

    Ajout bouton menu interactions

--- TestBomboclat/src/main/java/fr/amu/iut/MenuControl.java
+++ TestBomboclat/src/main/java/fr/amu/iut/MenuControl.java
@@ -24,4 +24,16 @@ public class MenuControl {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void quitScene(javafx.event.ActionEvent event) throws IOException {
+        //stage.close();
+    }
+
+    public void ParamButton(javafx.event.ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Parametre.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
